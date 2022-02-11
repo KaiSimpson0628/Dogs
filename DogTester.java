b3c7638ff7c98ec51e756d8c230c0adcf57d3219
KaiSimpson0628 <60585848+KaiSimpson0628@users.noreply.github.com>
2022-02-11 09:28:58 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -6,11 +6,12 @@ public static void main(String[] args) {
 Dog d1 = new Dog("Pal", 2018, "unspecified");
 Doodle l1 = new Doodle("Rocket", 2019, "Doodle", "doesn't shed");
 ShihTzu s1 = new ShihTzu("Hannibal", 2015, "ShihTzu", 8);
-
+GreyHound g1 = new GreyHound("Zoey", 2020, "Grey Hound", 43);
  ArrayList<Dog> dogsList = new ArrayList<>();
  dogsList.add(d1);
  dogsList.add(l1);
  dogsList.add(s1);
+  dogsList.add(g1);
  System.out.println(dogsList);
 }
 
